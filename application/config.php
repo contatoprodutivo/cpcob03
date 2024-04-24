a82dccb282333718aa0c08eff71531604f5dbc47
contatoprodutivo <contatoprodutivo@hotmail.com>
2024-04-24 16:59:13 -0300

    Versão 1

---
+++ application/config.php
@@ -0,0 +1,7 @@
+<?php
+$db_host	    = 'localhost';
+$db_user        = 'root';
+$db_password	= '';
+$db_name	    = 'cpcob_padrao';
+define('APP_URL', 'http://localhost/cpcob03');
+$_app_stage = 'Live'; // You can set this variable Live to Dev to enable ibilling Debug
